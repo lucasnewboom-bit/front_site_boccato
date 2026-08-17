dd2953f910e03707f6f718036f61eefcd3c12367
lucasnewboom-bit <lucasnewboom@gmail.com>
2026-08-17 20:33:18 -0300

    Add initial layout for index.php with navigation

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php include 'inc/header.inc.php'; ?>
+<div class= "container">
+    <div class ="side"> 
+        <section class="busca">
+            <h2>Buscar Item</h2>
+            <form>
+                <input type="search" name="busca">
+                <button>Buscar</button>
+            </form>
+        </section>
+<section class="menu_departamentos">
+    <h2>Departamentos</h2>
+    <nav>
+        <h3>Setor de Alimentos com frescura</h3>
+        <ul>
+            <li><a href="#">Hortifrúti</a>
+            <ul>
+                 <li><a href="#">Frutas</a></li>
+                 <li><a href="#">Legumes</a></li>
+                 <li><a href="#">Verduras</a></li>
+                 <li><a href="#">Temperos Frescos</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Padaria</a>
+           <ul>
+                 <li><a href="#">Pães</a></li>
+                 <li><a href="#">Bolos</a></li>
+                 <li><a href="#">Salgados</a></li>
+                 <li><a href="#">Doces</a></li>
+            </ul>
+       </li> 
+       <li><a href="#">Açougue e Peixaria</a>
+        <ul>
+                 <li><a href="#">Carnes Vermelhas</a></li>
+                 <li><a href="#">Peixes</a></li>
+                 <li><a href="#">Aves</a></li>
+                 <li><a href="#">Frutos do Mar</a></li>
+            </ul>  
+        </li>
+        <li><a href="#">Frios e Laticínios
+        <ul>
+                 <li><a href="#">Queijo/li>
+                 <li><a href="#">Presunto</a></li>
+                 <li><a href="#">Iogurte</a></li>
+                 <li><a href="#">Manteiga</a></li>
+                 <li><a href="#">Leite</a></li>
+            </ul>  
+        </li> 
+       </ul>
+        <h3>Setor de Despensa e Bebidas</h3>
+        <ul>
+            <li><a href="#">Mercearia</a>
+            <ul>
+                 <li><a href="#">Arroz</a></li>
+                 <li><a href="#">Feijão</a></li>
+                 <li><a href="#">Massas</a></li>
+                 <li><a href="#">Óleos</a></li>
+                 <li><a href="#">Açucas</a></li>
+                 <li><a href="#">Café</a></li>
+                 <li><a href="#">Enlatados</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Bebidas</a>
+           <ul>
+                 <li><a href="#">Águas</a></li>
+                 <li><a href="#">Sucos</a></li>
+                 <li><a href="#">Refrigerantes</a></li>
+                 <li><a href="#">Cervejas</a></li>
+                 <li><a href="#">Vinhos</a></li>
+                 <li><a href="#">Destilados</a></li>
+            </ul>
+       </li>
+               <li><a href="#">Congelados</a>
+           <ul>
+                 <li><a href="#">Pratos Prontos</a></li>
+                 <li><a href="#">Pizzas</a></li>
+                 <li><a href="#">Pão de Queijo</a></li>
+                 <li><a href="#">Vegetais congelados</a></li>
+            </ul>
+       </li>
+    </ul>
+       <h3>Setor de Uso Pessoal e de Casa</h3>
+        <ul>
+            <li><a href="#">Higene e Perfumaria</a>
+            <ul>
+                 <li><a href="#">Sabonetes</a></li>
+                 <li><a href="#">Shampoos</a></li>
+                 <li><a href="#">Cremes</a></li>
+                 <li><a href="#">Desodorantes</a></li>
+                 <li><a href="#">Pepel Higiênico</a></li>
+            </ul>
+        </li>
+           <li><a href="#">Limpeza</a>
+            <ul>
+                 <li><a href="#">Sabão em Pó</a></li>
+                 <li><a href="#">Desinfetante</a></li>
+                 <li><a href="#">Amaciante</a></li>
+                 <li><a href="#">Esponja</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Utilidade e Pet-shop</a>
+            <ul>
+                 <li><a href="#">Ração</a></li>
+                 <li><a href="#">Saches</a></li>
+                 <li><a href="#">Brinquedos</a></li>
+                 <li><a href="#">Petisco</a></li>
+                 <li><a href="#">Sacos de Lixo</a></li>
+                 <li><a href="#">Roupas Pet</a></li>
+            </ul>
+        </li>
+    </ul>
+    </nav>
+</section>
+    </div>
+<div class ="main">
+    <h1>Foto</h1>
+    </div>
+</div>
+<?php include 'inc/footer.inc.php'; ?>
+
+        
